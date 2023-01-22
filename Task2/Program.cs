98e5e034a5c08ee790a042cb202f1fda807c2ece
Dmitry <mangosovdima@mail.ru>
2023-01-22 16:39:45 +0300

    added sumNumber

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел от 1 до А
+int sumNumber(int arg)
+{
+    int result=0;
+for(int i=1;i<=arg;i++)
+{
+   result = result+i;
+   Console.WriteLine("Cумма чисел: "+result);
+}
+return result;
+}
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+sumNumber(A);
