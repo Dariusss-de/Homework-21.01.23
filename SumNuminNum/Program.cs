d325aa97b59d8e5b58c8484d53950de4ffe72a03
Dmitry <mangosovdima@mail.ru>
2023-01-22 18:20:39 +0300

    added SumNuminNum

---
+++ SumNuminNum/Program.cs
@@ -0,0 +1,18 @@
+﻿// Подсчитать сумму цифр в числе
+int SumNumber(int arg)
+{
+    int res = 0;
+    int sum=0;
+    while(arg>0)
+    {
+        res = res+ arg%10;
+        arg = arg/10;
+
+    }
+    Console.WriteLine("Сумма цифр = "+res);
+    return res;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+SumNumber(number);
