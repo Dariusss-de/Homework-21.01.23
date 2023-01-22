dfb8a414640ffa6e9798c8e9759bb7933676d5ae
Dmitry <mangosovdima@mail.ru>
2023-01-22 16:36:08 +0300

    added degreeCubeNumber

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти кубы чисел от 1 до N
+int degreeCube(int arg)
+{
+    int result=0;
+for(int i=1;i<=arg;i++)
+{
+   result = i*i*i;
+   Console.WriteLine("Куб числа: "+i+" = "+result);
+}
+return result;
+}
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+degreeCube(N);
+
+/* Найти кубы чисел от 1 до N с генератором случайных значений
+int degreeCube(int arg)
+{
+    int result=0;
+for(int i=1;i<=arg;i++)
+{
+   result = i*i*i;
+   Console.WriteLine("Куб числа: "+i+" = "+result);
+}
+return result;
+}
+int N = new Random().Next(1,25);
+degreeCube(N);
+*/
