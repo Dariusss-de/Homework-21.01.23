d039fc82c068f13e2a34a676175e9d35c0cfff77
Dmitry <mangosovdima@mail.ru>
2023-01-22 18:28:46 +0300

    added productNumber

---
+++ productNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+void ProductNumber (int arg)
+{
+    int result=1;
+    for(int i=1;i<=arg;i++)
+    {
+        result = result * i;
+    }
+     Console.WriteLine("Произведение числе = "+result);
+}
+Console.Write("Введите значение N: ");
+int number = int.Parse(Console.ReadLine());
+ProductNumber (number);
+
