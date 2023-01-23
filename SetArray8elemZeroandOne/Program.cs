45bb55195b52bc85df3c70a87c642e3988caffc8
Dmitry <mangosovdima@mail.ru>
2023-01-23 20:46:07 +0300

    Added SetArray

---
+++ SetArray8elemZeroandOne/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void SetArray(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+    int num =  new Random().Next(2);
+    array[i] = num;
+    }
+}
+void OutputOnTheSqreen(int[] array)
+{
+   foreach(int item in array)
+   {
+    Console.Write($" {item} ");
+   }
+}
+int[] arr = new int[8];
+SetArray(arr);
+OutputOnTheSqreen(arr);
