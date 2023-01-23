5e3dbc4c9d4da50da026e1afb7b2b026e16affc9
Dmitry <mangosovdima@mail.ru>
2023-01-23 20:02:13 +0300

    added task6

---
+++ cubeNumberEndDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int CubeNumber(int arg)
+{
+    int result = arg*arg*arg;
+    return result;
+}
+
+void FindNumber(int arg)
+{
+     int num = arg%10;
+     Console.WriteLine ("Последняя цифра числа = "+num);
+     if(num%2==0) Console.WriteLine ("Куб числа оканчивается на чётную цифру = "+arg);
+     else Console.WriteLine("Куб числа оканчивается на нечётную цифру");
+}
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = CubeNumber(number);
+FindNumber(result);
